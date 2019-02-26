1f15a1d0110927bde079a7cb33f5eb54b5cc6208
Suwen <577014284@qq,com>
2019-02-26 18:23:18 +0800

    1st experiment

---
+++ 1st_experiment/CarApplication/src/exp1/CarApplication.java
@@ -0,0 +1,12 @@
+package exp1;
+
+public class CarApplication {
+	   public static void main(String[] arguments) {
+	    Car car1=new Car();
+	    car1.speed=100;
+	    car1.showSpeed();
+	    car1.speedUp(10);
+	    car1.showSpeed();
+	    }
+
+}
